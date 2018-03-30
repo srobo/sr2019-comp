42564c052eb197bb5fcb5cb4161820aa7c159ea2
Peter Law <PeterJCLaw@gmail.com>
2018-03-30 21:52:09 +0100

    Add initial state
    Mostly copied from the 2017 Smallpeice repo, with tweaks.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Student Robotics 2019 Competition State
+
+Competition state for the 2019 Student Robotics competition.
---
+++ scoring/tests/test_scoring.py
@@ -0,0 +1,8 @@
+
+# Path hackery
+import os.path
+import sys
+ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, ROOT)
+
+from score import Scorer, InvalidScoresheetException
